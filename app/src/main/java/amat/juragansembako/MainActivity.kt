a176d008f23a33bdbd44313d37234d124d1ee86d
gadogado092 <gadogado092>
2022-10-22 12:24:23 +0800

    add bottom navigation

--- app/src/main/java/amat/juragansembako/MainActivity.kt
+++ app/src/main/java/amat/juragansembako/MainActivity.kt
@@ -1,43 +1,109 @@
 package amat.juragansembako
 
+import amat.juragansembako.ui.home.BottomNavItem
+import amat.juragansembako.ui.home.CashierScreen
+import amat.juragansembako.ui.home.OtherScreen
+import amat.juragansembako.ui.home.TransactionScreen
+import amat.juragansembako.ui.theme.JuraganSembakoTheme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import amat.juragansembako.ui.theme.JuraganSembakoTheme
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JuraganSembakoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                MainScreenView()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainScreenView() {
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = {
+            MyBottomNavigation(navController = navController)
+        }
+    ) {
+        NavigationGraph(navController = navController)
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun MyBottomNavigation(navController: NavController) {
+    val items = listOf(
+        BottomNavItem.Home,
+        BottomNavItem.Transaction,
+        BottomNavItem.Other
+    )
+    BottomNavigation {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+        items.forEach { item ->
+            BottomNavigationItem(
+                icon = { Icon(painterResource(id = item.icon), contentDescription = item.title) },
+                label = {
+                    Text(
+                        text = item.title,
+                        fontSize = 9.sp
+                    )
+                },
+                selectedContentColor = Color.Black,
+                unselectedContentColor = Color.Black.copy(0.4f),
+                alwaysShowLabel = true,
+                selected = currentRoute == item.screen_route,
+                onClick = {
+                    navController.navigate(item.screen_route) {
+
+                        navController.graph.startDestinationRoute?.let { screen_route ->
+                            popUpTo(screen_route) {
+                                saveState = true
+                            }
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+
+                })
+        }
+    }
+}
+
+@Composable
+fun NavigationGraph(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = BottomNavItem.Home.screen_route) {
+        composable(BottomNavItem.Home.screen_route) {
+            CashierScreen()
+        }
+        composable(BottomNavItem.Transaction.screen_route) {
+            TransactionScreen()
+        }
+        composable(BottomNavItem.Other.screen_route) {
+            OtherScreen()
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     JuraganSembakoTheme {
-        Greeting("Android")
+        MainScreenView()
     }
 }
---
+++ app/src/main/java/amat/juragansembako/ui/home/BottomNavItem.kt
@@ -0,0 +1,13 @@
+package amat.juragansembako.ui.home
+
+import amat.juragansembako.R
+
+sealed class BottomNavItem(var title: String, var icon: Int, var screen_route: String) {
+
+    object Home : BottomNavItem("Home", R.drawable.ic_baseline_home_24, "home")
+    object Transaction : BottomNavItem("Transaksi", R.drawable.ic_baseline_home_24, "transaction")
+    object Other : BottomNavItem("Lainnya", R.drawable.ic_baseline_home_24, "Other")
+//    object AddPost: BottomNavItem("Post",R.drawable.ic_post,"add_post")
+//    object Notification: BottomNavItem("Notification",R.drawable.ic_notification,"notification")
+//    object Jobs: BottomNavItem("Jobs",R.drawable.ic_job,"jobs")
+}
